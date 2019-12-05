67c15c0eb84fcb69c37ec62555bdf52743db506e
Anthony La Sala <anthony.lasala@outlook.com.au>
2019-12-05 17:31:03 +1100

    clean up of default project and added axios

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <span>Working</span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
