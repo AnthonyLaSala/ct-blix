2dfa3dcffa1cac671dbb4ce3deddb5208bc1e551
Anthony La Sala <anthony.lasala@outlook.com.au>
2019-12-05 17:39:20 +1100

    import api

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="hello">
-    <span>Working</span>
+    <span>Working {{data}}</span>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
   name: 'Home',
-  
+  data() {
+    return {
+      data: null,
+    }
+  },
+  mounted () {
+    axios
+      .get('https://data.gov.au/data/api/3/action/datastore_search_sql?sql=SELECT%20*%20from%20%229e26683b-6b30-424e-ace7-59047d811d1c%22')
+      .then(response => (this.data = response.data.result.records))
+  },
 }
 </script>
 
